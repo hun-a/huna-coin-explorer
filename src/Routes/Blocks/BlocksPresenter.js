2b0eafb9a2b54799a2af1f82f130660f7a96b359
seunghun <seunghun.chan@gmail.com>
2019-03-18 01:46:58 +0900

    Navigation and Routes

---
+++ src/Routes/Blocks/BlocksPresenter.js
@@ -0,0 +1,6 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const BlockPresenter = () => "blocks";
+
+export default BlockPresenter;
---
+++ src/Routes/Transactions/TransactionsPresenter.js
@@ -0,0 +1,6 @@
+import react from "react";
+import PropTypes from "prop-types";
+
+const TransactionsPresenter = () => "txs";
+
+export default TransactionsPresenter;
