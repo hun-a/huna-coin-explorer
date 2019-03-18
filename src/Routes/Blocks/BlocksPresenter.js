99a2967499603bc48a104f87660b662c3ed2a710
seunghun <seunghun.chan@gmail.com>
2019-03-19 02:00:47 +0900

    Implement the showing page of blocks and transactions

--- src/Routes/Blocks/BlocksPresenter.js
+++ src/Routes/Blocks/BlocksPresenter.js
@@ -1,6 +1,40 @@
-import React from "react";
+import React, { Fragment } from "react";
+import styled from "styled-components";
 import PropTypes from "prop-types";
+import { makeDateSexy } from "../../utils";
+import {
+  Card,
+  BlocksHeader,
+  BlocksRow
+} from "Components/Shared";
 
-const BlockPresenter = () => "blocks";
+const TableContainer = styled.div`
+  margin-top: 50px;
+  margin-bottom: 100px;
+`;
+
+const BlockPresenter = ({ blocks }) => (
+  <Fragment>
+    <TableContainer>
+      <h2>Latest Blocks</h2>
+      <Card>
+        <BlocksHeader />
+        {blocks.map((block, index) => (
+          <BlocksRow
+            index={block.index}
+            hash={block.hash}
+            timestamp={makeDateSexy(block.timestamp)}
+            difficulty={block.difficulty}
+            key={index}
+          />
+        ))}
+      </Card>
+    </TableContainer>
+  </Fragment>
+);
+
+BlockPresenter.propTypes = {
+  blocks: PropTypes.array.isRequired
+};
 
 export default BlockPresenter;
--- src/Routes/Transactions/TransactionsPresenter.js
+++ src/Routes/Transactions/TransactionsPresenter.js
@@ -1,6 +1,41 @@
-import react from "react";
+import React, { Fragment } from "react";
+import styled from "styled-components";
+import sum from "lodash.sum";
 import PropTypes from "prop-types";
+import { makeDateSexy } from "../../utils";
+import {
+  Card,
+  TxHeader,
+  TxRow
+} from "Components/Shared";
 
-const TransactionsPresenter = () => "txs";
+const TableContainer = styled.div`
+  margin-top: 50px;
+  margin-bottom: 100px;
+`;
+
+const TransactionsPresenter = ({ transactions }) => (
+  <Fragment>
+    <TableContainer>
+      <h2>Latest Transactions</h2>
+      <Card>
+        <TxHeader />
+        {transactions.map((transaction, index) => (
+          <TxRow
+            timestamp={makeDateSexy(transaction.timestamp)}
+            id={transaction.id}
+            insOuts={`${transaction.txIns.length}/${transaction.txOuts.length}`}
+            amount={sum(transaction.txOuts.map(txOut => txOut.amount))}
+            key={index}
+          />
+        ))}
+      </Card>
+    </TableContainer>
+  </Fragment>
+);
+
+TransactionsPresenter.propTypes = {
+  transactions: PropTypes.array.isRequired
+};
 
 export default TransactionsPresenter;
